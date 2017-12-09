d009059a54a7017a592f7c283fc58e35cc142f8c
Pavel Parfenov <pavelparfenov112@GMAIL.COM>
2017-12-09 15:27:14 -0500

    added blackjackcomponent

---
+++ src/app/blackjack/blackjack.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blackjack',
+  templateUrl: './blackjack.component.html',
+  styleUrls: ['./blackjack.component.css']
+})
+export class BlackJackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
