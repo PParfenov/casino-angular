aee8536748fcc5717a92addfc6a58c237ce1361c
zan <yzcheema11@gmail.com>
2017-12-09 22:24:41 -0500

    "It doesn't work."

--- src/app/blackjack/blackjack.component.spec.ts
+++ src/app/blackjack/blackjack.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BlackjackComponent } from './blackjack.component';
+import { HandComponent } from './blackjackHand/hand.component';
 
 describe('BlackjackComponent', () => {
   let component: BlackjackComponent;
@@ -8,7 +9,7 @@ describe('BlackjackComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BlackjackComponent ]
+      declarations: [ BlackjackComponent, HandComponent ]
     })
     .compileComponents();
   }));
@@ -21,5 +22,14 @@ describe('BlackjackComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should give card to player', () => {
+    component.hitMe();
+    expect(component.player.cards.length).toEqual(1);
+  });
+
+  it('should have a full deck', () => {
+    expect(component.deck.getDeck().length).toEqual(52);
   });
 });
