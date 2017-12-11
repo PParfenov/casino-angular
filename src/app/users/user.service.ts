46974e15f55f4a910e0c5ec2ca1e5b8b00a0ad17
Pavel <PavelParfenov112@gmail.com>
2017-12-10 19:53:19 -0500

    meh

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { User } from '../user';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Injectable()
-export class UserService {
-
+export class UserService implements OnInit {
+  
   private user: User = new User;
+  
+  ngOnInit(): void {
+    this.user.name = "No name";
+    this.user.money = 0;
+  }
 
   constructor() { }
 
